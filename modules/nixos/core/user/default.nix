83733d3d41dfd60300c5bdf689b815bc08c09ef4
miigha <tech@valkonen.cc>
2026-07-01 18:25:10 +0300

    Added ssh key to main user

--- modules/nixos/core/user/default.nix
+++ modules/nixos/core/user/default.nix
@@ -1,8 +1,5 @@
 {
-  flake.modules.nixos.user = {
-    user,
-    ...
-  }: {
+  flake.modules.nixos.user = {user, ...}: {
     users.users = {
       root = {
         password = "change-me-now";
@@ -12,6 +9,9 @@
         isNormalUser = true;
         description = user.username;
         password = "change-me-now";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINod4UhBmnXQ56HFrHNMTPgXmegaOfUsOOnYSqhxrzIf tech@valkonen.cc"
+        ];
       };
     };
   };
