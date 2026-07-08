2a50100083cbd38d8fc7aad48cf1804c673109e0
miigha <tech@valkonen.cc>
2026-07-08 13:35:08 +0300

    extended git settings

--- modules/home/dev-base/git.nix
+++ modules/home/dev-base/git.nix
@@ -1,19 +1,33 @@
 {
-  flake.modules.homeManager.git = {
-    user,
-    ...
-  }: {
+  flake.modules.homeManager.git = {user, ...}: {
     programs.git = {
       enable = true;
-      settings.user = {
-        name = user.username;
-        inherit (user) email;
-      };
-      settings.alias = {
-        st = "status";
-        co = "checkout";
-        cob = "checkout -b";
-        com = "checkout main";
+
+      settings = {
+        user = {
+          name = user.username;
+          inherit (user) email;
+        };
+        alias = {
+          ec = "config --global -e";
+          st = "status";
+          co = "checkout";
+          cob = "checkout -b";
+          com = "checkout master";
+          cobf = "!f() { git up; git checkout -b feature/$1 ; }; f";
+          up = "!git pull --rebase --prune $@";
+          done = "!\"git com; git up; git bc\"";
+        };
+        core = {
+          editor = "nvim";
+          commentchar = ";";
+          hooksPath = "~/git-hooks";
+          autocrlf = "false";
+          pager = "delta";
+        };
+        interactive.diffFilter = "delta --color-only";
+        delta.navigate = "true";
+        merge.conflictStyle = "zdiff3";
       };
     };
   };
--- modules/home/dev-extra/default.nix
+++ modules/home/dev-extra/default.nix
@@ -1,5 +1,6 @@
 {config, ...}: {
   flake.modules.homeManager.dev-extra.imports = with config.flake.modules.homeManager; [
     extra-packages
+    git-hooks
   ];
 }
