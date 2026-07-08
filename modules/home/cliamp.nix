81f3c357d37ce025381f3f3a8de2a0517cfb7d8d
miigha <tech@valkonen.cc>
2026-07-08 13:34:50 +0300

    cliamp config modification

--- modules/home/cliamp.nix
+++ modules/home/cliamp.nix
@@ -7,6 +7,8 @@
     home.packages = pkgs.lib.mkAfter [pkgs.cliamp pkgs.yt-dlp];
     home.file.".config/cliamp/config.toml".text = ''
       theme = ""
+      provider = "ytmusic"
+      visualizer = "Wave"
 
       [ytmusic]
       client_id = "${secrets.ytmusic.client_id}"
