6e2780df76a5910f1defb51b9fa382bec34dc014
miikka <miikka.valkonen@if.fi>
2026-07-20 12:59:50 +0000

    Tmux - minor changes

--- modules/home/dev-base/tmux.nix
+++ modules/home/dev-base/tmux.nix
@@ -5,7 +5,6 @@
       plugins = with pkgs.tmuxPlugins; [
         minimal-tmux-status
         yank
-        copy-toolkit
         pain-control
       ];
 
@@ -15,11 +14,13 @@
         set -sg escape-time 10
         set -g focus-events on
         set -g default-terminal "screen-256color"
+        set-option -a terminal-features 'xterm-256color:RGB'
         bind r source-file ~/.config/tmux/tmux.conf \; display-message "Config reloaded..."
         bind -T copy-mode-vi v send-keys -X begin-selection
         bind d kill-session
         bind -n M-h previous-window
         bind -n M-l next-window
+        bind -n M-c copy-mode
       '';
     };
   };
